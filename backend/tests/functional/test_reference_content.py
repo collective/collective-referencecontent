1659d19a166fc090957ab127a837c1f46cc082e9
Andrea Cecchi <andrea.cecchi85@gmail.com>
2025-11-08 18:52:46 +0100

    update tests

--- backend/tests/functional/test_reference_content.py
+++ backend/tests/functional/test_reference_content.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from plone import api
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
@@ -27,6 +28,16 @@ def test_reference_content_proxies_catalog_metadata(request, functional):
     )
     doc_intid = intids.getId(proxied_doc)
 
+    proxied_event = api.content.create(
+        container=portal,
+        type="Event",
+        id="proxied-even",
+        title="Proxied Event",
+        start=datetime(2024, 4, 22, 12, 0),
+        end=datetime(2024, 4, 22, 12, 0),
+    )
+    event_intid = intids.getId(proxied_event)
+
     reference_content = api.content.create(
         container=portal,
         type="ReferenceContent",
@@ -34,19 +45,28 @@ def test_reference_content_proxies_catalog_metadata(request, functional):
         title="My Reference",
         proxied_content=[RelationValue(doc_intid)],
     )
+    reference_event = api.content.create(
+        container=portal,
+        type="ReferenceContent",
+        id="reference-event",
+        title="My Reference Event",
+        proxied_content=[RelationValue(event_intid)],
+    )
     transaction.commit()
 
-    brains = catalog(UID=reference_content.UID())
-    assert len(brains) == 1
+    doc_ref_brain = catalog(UID=reference_content.UID())[0]
+    event_ref_brain = catalog(UID=reference_event.UID())[0]
 
-    brain = brains[0]
-
-    # The brain's metadata should match the proxied document's data
-    assert brain.Title == proxied_doc.title
-    assert brain.Description == proxied_doc.description
-    assert brain.Subject == proxied_doc.subject
-    assert brain.portal_type == proxied_doc.portal_type
+    # The doc_ref_brain's metadata should match the proxied document's data
+    assert doc_ref_brain.Title == proxied_doc.title
+    assert doc_ref_brain.Description == proxied_doc.description
+    assert doc_ref_brain.Subject == proxied_doc.subject
+    assert doc_ref_brain.portal_type == proxied_doc.portal_type
 
     # The id should be from the ReferenceContent itself
-    assert brain.getId == "reference-content"
-    assert reference_content.UID() == brain.UID
+    assert doc_ref_brain.getId == "reference-content"
+    assert reference_content.UID() == doc_ref_brain.UID
+
+    # reference event also have start/end metadata
+    assert event_ref_brain.start == proxied_event.start
+    assert event_ref_brain.end == proxied_event.end
